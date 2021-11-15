aebf07808e50e8da311e3a1fb8cf3fc5072d2bdd
Johannes Steinhausen <johannes.steinhausen@gmail.com>
2021-11-16 00:28:13 +0100

    Class CollisionTest.java updated

--- Hexenspiel/src/main/java/trinat/hexenspiel/hexenspiel/CollisionTest.java
+++ Hexenspiel/src/main/java/trinat/hexenspiel/hexenspiel/CollisionTest.java
@@ -4,35 +4,38 @@ import javafx.scene.shape.Rectangle;
 
 public class CollisionTest {
     private Rectangle myRectangle= new Rectangle();
-    private double X;
-    private double Y;
-    private double Width;
-    private double Height;
-
+    private Rectangle rectangle= new Rectangle();
 
     public CollisionTest(double myX, double myY, double myWidth, double myHeight, double x, double y, double width, double height) {
         myRectangle.setX(myX);
         myRectangle.setY(myY);
         myRectangle.setWidth(myWidth);
         myRectangle.setHeight(myHeight);
-        X = x;
-        Y = y;
-        Width = width;
-        Height = height;
+        myRectangle.setX(x);
+        myRectangle.setY(y);
+        myRectangle.setWidth(width);
+        myRectangle.setHeight(height);
     }
     public CollisionTest(Rectangle myRectangle,Rectangle rectangle) {
-        this.myRectangle.setX(myRectangle.getX());
-        this.myRectangle.setY(myRectangle.getY());
-        this.myRectangle.setWidth(myRectangle.getWidth());
-        this.myRectangle.setHeight(myRectangle.getHeight());
-        X = rectangle.getX();
-        Y = rectangle.getY();
-        Width = rectangle.getWidth();
-        Height = rectangle.getHeight();
+        this.myRectangle=myRectangle;
+        this.rectangle=rectangle;
     }
 
-    public static boolean testCollision(Rectangle myRectangle,Rectangle rectangle){
+    public CollisionTest(Rectangle myRectangle) {
+        this.myRectangle = myRectangle;
+    }
+
+    public static boolean testCollision(Rectangle myRectangle, Rectangle rectangle){
         return myRectangle.intersects(rectangle.getX(),rectangle.getY(),rectangle.getWidth(),rectangle.getHeight());
+    }
+    public boolean testCollision(Rectangle rectangle){
+        return this.myRectangle.intersects(rectangle.getX(),rectangle.getY(),rectangle.getWidth(),rectangle.getHeight());
+    }
+    public boolean testCollison(double x, double y, double width, double height){
+        return this.myRectangle.intersects(x,y,width,height);
+    }
+    public boolean testCollision(){
+        return this.myRectangle.intersects(this.rectangle.getX(),this.rectangle.getY(),this.rectangle.getWidth(),this.rectangle.getHeight());
     }
 
 
