a79d97c17df4a95614bedac8ef179c96228a7cda
Joanna Senn <joanna.senn@students.fhnw.ch>
2021-11-17 17:53:44 +0100

    new timeline

--- Hexenspiel/src/main/java/witchdash/Main.java
+++ Hexenspiel/src/main/java/witchdash/Main.java
@@ -1,15 +1,15 @@
 package witchdash;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.scene.*;
-import javafx.scene.control.*;
-import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
-import java.util.Optional;
 
 public class Main extends Application {
 
@@ -23,66 +23,36 @@ public class Main extends Application {
     public int sceneSizeX = 600;
     public int sceneSizeY = 500;
 
+
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Witchdash");
 
         Group root = new Group();
-        Scene scene = new Scene(root,sceneSizeX,sceneSizeY, Color.BURLYWOOD);
+        Scene scene = new Scene(root, sceneSizeX, sceneSizeY, Color.WHITE);
         stage.setScene(scene);
 
-        Button butt = new Button("Pause");
-        this.witch = new Witch(posX,posY, width, height);
-        root.getChildren().addAll(witch, butt);
+
+        this.witch = new Witch(posX, posY, width, height);
+        root.getChildren().add(witch);
         stage.show();
-        //Pause Menu
-        butt.setOnAction(e -> pause());
 
-        scene.setOnKeyPressed(e -> {
-            if(e.getCode().equals(KeyCode.W) || e.getCode().equals(KeyCode.Z)) {
-                if (posX>=0){
-                    Witch.Rectangle_Translation(witch,posX,posY,-1,0);
-                }
-            }
-            else if (e.getCode().equals(KeyCode.S)) {
-                if (posX<=sceneSizeX-height){
-                    Witch.Rectangle_Translation(witch,posX,posY,1,0);
-                }
-            }
-        });
+        // generate random numbers within 40 and 460
+        int pumpkinY = (int) (Math.random() * sceneSizeY - 40) + 40;
+        int pumpkinX = sceneSizeX;
+        this.pumpkin = new Pumpkin(pumpkinX, pumpkinY, 40, 40, 2, 2);
+        root.getChildren().add(pumpkin);
+        stage.show();
 
-
-        /* //für auf und abbewegung
-        .addEventHandler(KeyEvent.KEY_PRESSED,
-                e -> {pause();}
-                );
-
-         */
-    }
-
-
-    public void pause() {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("PAUSEMENU");
-        alert.setHeaderText("Game is paused!");
-        alert.setContentText("Choose your option.");
-
-        ButtonType buttonTypeOne = new ButtonType("Continue");
-        ButtonType buttonTypeTwo = new ButtonType("Rules");
-        ButtonType buttonTypeCancel = new ButtonType("Close Game", ButtonBar.ButtonData.CANCEL_CLOSE);
-
-        alert.getButtonTypes().setAll(buttonTypeOne, buttonTypeTwo, buttonTypeCancel);
-
-        Optional<ButtonType> result = alert.showAndWait();
-        if (result.get() == buttonTypeOne){
-            // ... user chose "One"
-        } else if (result.get() == buttonTypeTwo) {
-            // Open Rules
-        } else {
-            //Close the game
-            Platform.exit();
+        //Movement pumpkin
+        while ((pumpkinX >= 0)) {
+            Timeline timeline = new Timeline(
+                    new KeyFrame(Duration.ZERO, new KeyValue(pumpkin.translateXProperty(), pumpkinX)),
+                    new KeyFrame(Duration.minutes(2), new KeyValue(pumpkin.translateXProperty(), pumpkinX - 10000))
+            );
+            timeline.play();
+            stage.show();
+            pumpkinX=pumpkinX-10;
         }
     }
-
-
 }
--- Hexenspiel/src/main/java/witchdash/Pumpkin.java
+++ Hexenspiel/src/main/java/witchdash/Pumpkin.java
@@ -1,4 +1,64 @@
 package witchdash;
 
-public class Pumpkin {
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Pumpkin extends Rectangle{
+
+    public Pumpkin (int positionX, int positionY, int Width, int Height, int ArcWidth, int ArcHeight){
+        Rectangle pumpkin = new Rectangle();
+        this.setX(positionX);
+        this.setY(positionY);
+        this.setWidth(Width);
+        this.setHeight(Height);
+        this.setArcWidth(ArcWidth);
+        this.setArcHeight(ArcHeight);
+        this.setFill(Color.ORANGE);
+    }
+    public static Rectangle Rectangle_Translation(Rectangle rInterior,int changementX){
+        //Setting the X,Y,Z coordinates to apply the translation
+        rInterior.setX(rInterior.getX()+changementX);
+        return rInterior;
+
+    }
+
+
+
 }
+
+/*
+import javafx.scene.paint.Color;
+        import javafx.scene.shape.Rectangle;
+
+public class Obstacle {
+    public static Rectangle Hit_box_obstacle (int positionX, int positionY, int Width, int Height, int ArcWidth, int ArcHeight){
+        //creating a shape
+        Rectangle r = new Rectangle();
+        r.setX(positionX);
+        r.setY(positionY);
+        r.setWidth(Width);
+        r.setHeight(Height);
+        r.setArcWidth(ArcWidth);
+        r.setArcHeight(ArcHeight);
+        r.setFill(Color.RED);
+        //returning the shape
+        return r;
+    }
+
+    public static Image Obstacle_image (Stage primaryStage){
+        //calling an image
+        InputStream iconStream = Obstacle.class.getResourceAsStream("src/main/resources/trinat/hexenspiel/hexenspiel/witch.png");
+        Image witch = new Image(iconStream);
+        //adding tne image to the stage
+        primaryStage.getIcons().add(witch);
+        //returning the image
+        return witch;
+    }
+
+    public static Rectangle Rectangle_Translation(Rectangle rInterior,int changementX){
+        //Setting the X,Y,Z coordinates to apply the translation
+        rInterior.setX(rInterior.getX()+changementX);
+        return rInterior;
+
+    }
+    */
