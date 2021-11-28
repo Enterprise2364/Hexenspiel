6e9d45b6caaf5e9244985e9612320ddd3b0ab947
Johannes Steinhausen <johannes.steinhausen@gmail.com>
2021-11-28 17:34:17 +0100

    GameEngine creates Pumpkins and a witch object+movePumkins method in Game Engine works+ testCollision method in Game Engine works

--- Hexenspiel/src/main/java/trinat/hexenspiel/hexenspiel/WitchDashEngine.java
+++ Hexenspiel/src/main/java/trinat/hexenspiel/hexenspiel/WitchDashEngine.java
@@ -1,32 +1,41 @@
 package trinat.hexenspiel.hexenspiel;
-
+import javafx.scene.Group;
 import javafx.stage.Stage;
-
 import java.util.ArrayList;
 
 public class WitchDashEngine {
     private Stage witchDashMainEngineStage;
     private ArrayList<Pumpkin>pumpkins=new ArrayList<>();
+    private Witch witch=new Witch();
 
-    public WitchDashEngine(Stage stage,int obstacleNumber){
+    public WitchDashEngine(Stage stage,int obstacleNumber,int witchSpeed,int pumpkinSpeed){
         this.witchDashMainEngineStage = stage;
         for(int i=0;i<obstacleNumber;i++){
             pumpkins.add(new Pumpkin(this.witchDashMainEngineStage.getWidth(),
                                         this.witchDashMainEngineStage.getY(),
                                         this.witchDashMainEngineStage.getHeight()));
-            //pumpkins.get(i).getRectangle().getX()-pumpkins.get(i).getRectangle().getWidth();
-
         }
     }
 
     public void movePumpkins(){
-        for(Pumpkin pumpkin: pumpkins) {
-            pumpkin.moveLeft();
+        for(int i=0;i<this.pumpkins.size();i++) {
+            pumpkins.get(i).moveLeft();
         }
     }
 
+
+
     public void moveFigures(){
 
+    }
+
+    public boolean testCollision(){
+        for(int i=0;i<this.pumpkins.size();i++){
+            if (this.witch.testCollision(this.pumpkins.get(i).getRectangle())) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public Stage getWitchDashMainEngineStage() {
@@ -43,5 +52,13 @@ public class WitchDashEngine {
 
     public void setPumpkins(ArrayList<Pumpkin> pumpkins) {
         this.pumpkins = pumpkins;
+    }
+
+    public Witch getWitch() {
+        return witch;
+    }
+
+    public void setWitch(Witch witch) {
+        this.witch = witch;
     }
 }
