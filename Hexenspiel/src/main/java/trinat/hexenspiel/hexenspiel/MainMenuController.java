0afd48843f5b4c0041fb90456ec5364a37bd3ba8
edwardhaynes <edward.haynes@students.fhnw.ch>
2021-12-01 21:15:35 +0100

    1 dec 2021 - apres Brice & Nico

--- Hexenspiel/src/main/java/trinat/hexenspiel/hexenspiel/MainMenuController.java
+++ Hexenspiel/src/main/java/trinat/hexenspiel/hexenspiel/MainMenuController.java
@@ -1,13 +1,8 @@
 package trinat.hexenspiel.hexenspiel;
 
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class MainMenuController {
 
@@ -15,18 +10,14 @@ public class MainMenuController {
         public Button new_game;
         @FXML
         public Button quit;
-        @FXML
-        private Label welcomeText;
 
-        @FXML
+    @FXML
         public void newGame() {
             WitchDashMain.launch();
         }
 
         @FXML
         public void quit() {
-            WitchDashMain.mainMenuStage.close();
-            WitchDashMain.witchDashMainStage.close();
             System.exit(0);
     }
 
